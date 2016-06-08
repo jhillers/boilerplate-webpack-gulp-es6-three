c5a39d9ee186c8b2dacc31d318e493fdccdf286e
jose.hillers <jose.hillers@gameys.co.uk>
2016-06-08 18:18:26 +0100

    Removed es6 loader.

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,7 @@ module.exports = {
             {
                 test: /\.(png|woff|woff2|eot|ttf|svg)$/,
                 loader: 'url-loader?limit=100000'
-            },
-            {
-                test: /\.js$/,
-                loader: 'es6-loader'
             }
-
         ]
     }
 };
